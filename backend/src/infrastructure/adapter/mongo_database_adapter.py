ad28899eda409ca232c1710aa82c5600a8f5c529
matiar <110628257+Matiar15@users.noreply.github.com>
2026-01-24 20:47:18 +0100

    #4 feat(database): enhance MongoDB adapter with Metadata support
    - Refactored `DatabasePort` to include `Metadata` specific operations.
    - Updated `MongoDatabaseAdapter` to utilize `Settings` for dynamic MongoDB connection initialization.
    - Implemented stricter type hints with `Metadata` for `create` method.

--- backend/src/infrastructure/adapter/mongo_database_adapter.py
+++ backend/src/infrastructure/adapter/mongo_database_adapter.py
@@ -3,16 +3,26 @@ import typing
 import pymongo
 
 from backend.src.infrastructure.port.database_port import DatabasePort
+from backend.src.metadata.domain.metadata import Metadata
+from backend.src.settings import Settings
 
 T = typing.TypeVar("T")
 
 
-class MongoDatabaseAdapter(DatabasePort[T], typing.Generic[T]):
-    def __init__(self, mongo_client: pymongo.AsyncMongoClient[T]):
-        self.mongo_client = mongo_client
+class MetadataMongoDatabaseAdapter(DatabasePort[Metadata]):
+    def __init__(
+        self,
+        settings: Settings,
+    ):
+        connection_string = f"mongodb+srv://{settings.database.user}:{settings.database.password}@{settings.database.host}/{settings.database.name}"
+        self.mongo_client: pymongo.AsyncMongoClient[Metadata] = (
+            pymongo.AsyncMongoClient(
+                connection_string,
+                maxPoolSize=20,
+                maxIdleTimeMS=30_000,
+            )
+        )
 
-
-    async def create(self, collection_name: str, domain: T) -> T:
+    async def create(self, collection_name: str, domain: Metadata) -> Metadata:
         collection = self.mongo_client[collection_name]
         return await collection.insert_one(domain)
-
--- backend/src/infrastructure/port/database_port.py
+++ backend/src/infrastructure/port/database_port.py
@@ -3,6 +3,8 @@ import typing
 
 T = typing.TypeVar("T")
 
+
 class DatabasePort(abc.ABC, typing.Generic[T]):
+    @abc.abstractmethod
     async def create(self, collection_name: str, domain: T) -> T:
         """Create a new domain object. Returns the created object."""
