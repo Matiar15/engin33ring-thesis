be0ac3ee35a5ff7d76f579e4ecb2e986dbd64700
matiar <110628257+Matiar15@users.noreply.github.com>
2026-01-25 12:13:52 +0100

    #4 feat(metadata): enforce stricter model validation
    - Updated `Metadata` model configuration to populate fields by name and forbid extra fields.

--- backend/src/metadata/domain/metadata.py
+++ backend/src/metadata/domain/metadata.py
@@ -7,3 +7,8 @@ PyObjectId = typing.Annotated[str, pydantic.BeforeValidator(str)]
 
 class Metadata(pydantic.BaseModel):
     id: PyObjectId | None = pydantic.Field(alias="_id", default=None)
+
+    model_config = {
+        "populate_by_name": True,
+        "extra": "forbid",
+    }
