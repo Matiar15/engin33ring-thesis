6438f30199c9df4190ebc22046e0238ce9aa7fab
matiar <110628257+Matiar15@users.noreply.github.com>
2026-01-25 12:13:29 +0100

    #4 feat(database): enhance MongoDB adapter with logging and async support
    - Added logging for MongoDB connection lifecycle.
    - Updated `create` method to support asynchronous insertions and return the inserted ID.
    - Refined MongoDB client initialization with collection name binding.

--- backend/src/infrastructure/adapter/mongo_database_adapter.py
+++ backend/src/infrastructure/adapter/mongo_database_adapter.py
@@ -1,12 +1,13 @@
-import typing
-
+import logging
 import pymongo
+
+from pymongo.asynchronous.database import AsyncDatabase
 
 from backend.src.infrastructure.port.database_port import DatabasePort
 from backend.src.metadata.domain.metadata import Metadata
 from backend.src.settings import Settings
 
-T = typing.TypeVar("T")
+_logger = logging.getLogger(__name__)
 
 
 class MetadataMongoDatabaseAdapter(DatabasePort[Metadata]):
@@ -15,14 +16,24 @@ class MetadataMongoDatabaseAdapter(DatabasePort[Metadata]):
         settings: Settings,
     ):
         connection_string = f"mongodb+srv://{settings.database.user}:{settings.database.password}@{settings.database.host}/{settings.database.name}"
-        self.mongo_client: pymongo.AsyncMongoClient[Metadata] = (
+
+        _logger.info(f"Connecting to MongoDB at {settings.database.host}...")
+        self.mongo_client: AsyncDatabase[Metadata] = (
             pymongo.AsyncMongoClient(
                 connection_string,
                 maxPoolSize=20,
                 maxIdleTimeMS=30_000,
             )
+        )[settings.database.name]
+        _logger.info("Connected to MongoDB.")
+        self.collection_name = "metadata"
+
+    async def create(self, domain: Metadata,) -> str:
+        insert_one_result = await self.mongo_client[self.collection_name].insert_one(
+            domain.model_dump(
+                by_alias=True,
+                exclude_unset=True,
+            )
         )
 
-    async def create(self, collection_name: str, domain: Metadata) -> Metadata:
-        collection = self.mongo_client[collection_name]
-        return await collection.insert_one(domain)
+        return str(insert_one_result.inserted_id)
