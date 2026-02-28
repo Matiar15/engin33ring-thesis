70326271b9f5f3fce0650e10868dff7cee76634d
matiar <110628257+Matiar15@users.noreply.github.com>
2026-02-28 17:54:15 +0100

    #4 refactor(frame): rename DatabasePort to FramePort
    - Renamed `DatabasePort` to `FramePort` for improved alignment with the frame domain.
    - Updated method signature to use `Frame` type for the `create` method.

--- backend/src/frame/application/frame_port.py
+++ backend/src/frame/application/frame_port.py
@@ -1,10 +1,12 @@
 import abc
 import typing
 
+from backend.src.frame.domain.frame import Frame
+
 T = typing.TypeVar("T")
 
 
-class DatabasePort(abc.ABC, typing.Generic[T]):
+class FramePort(abc.ABC):
     @abc.abstractmethod
-    async def create(self, domain: T) -> str:
+    async def create(self, frame: Frame) -> str:
         """Create a new domain object. Returns ID of the created object."""
