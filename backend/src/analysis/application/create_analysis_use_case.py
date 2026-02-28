3c738d12c1694ee6b76afb44b34748a9d6c2de70
matiar <110628257+Matiar15@users.noreply.github.com>
2026-02-28 20:58:12 +0100

    #4 refactor(analysis): update `CreateAnalysisUseCase` response type
    - Changed `create` method to return `AnalysisResponse` instead of `FrameResponse` for consistency with the domain model.
    - Updated logging messages and `map_to_response` to reflect the new response format.

--- backend/src/analysis/application/create_analysis_use_case.py
+++ backend/src/analysis/application/create_analysis_use_case.py
@@ -1,12 +1,8 @@
 import logging
 
-from backend.src.analysis.api.model import AnalysisPayload
+from backend.src.analysis.api.model import AnalysisPayload, AnalysisResponse, map_to_response
 from backend.src.analysis.application.analysis_port import AnalysisPort
 from backend.src.analysis.domain.analysis import Analysis
-from backend.src.frame.api.model import (
-    FrameResponse,
-    map_to_response,
-)
 
 _logger = logging.getLogger(__name__)
 
@@ -18,11 +14,11 @@ class CreateAnalysisUseCase:
     ):
         self.analysis_port = analysis_port
 
-    async def create(self, analysis_payload: AnalysisPayload) -> FrameResponse:
+    async def create(self, analysis_payload: AnalysisPayload) -> AnalysisResponse:
         _logger.info(f"Creating analysis for user: {analysis_payload.user_id}...")
         inserted_id: str = await self.analysis_port.create(
             analysis=Analysis.from_payload(analysis_payload.model_dump())
         )
 
-        _logger.info(f"Frame created with id: {inserted_id}")
-        return map_to_response()
+        _logger.info(f"Analysis created with id: {inserted_id}")
+        return map_to_response(inserted_id)
