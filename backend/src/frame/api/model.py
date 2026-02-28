66fae4c946bc2bc68f94625520f5daa5189a961e
matiar <110628257+Matiar15@users.noreply.github.com>
2026-02-28 17:51:51 +0100

    #4 refactor(api): replace metadata models with frame models
    - Removed `MetadataRequest` and `MetadataResponse` models, including their mapping logic.
    - Added `FramePayload`, `FrameResponse`, and `BoundingBox` models for frame data processing.
    - Introduced `map_to_response` function in the frame API for consistent response structure.

---
+++ backend/src/frame/api/model.py
@@ -0,0 +1,22 @@
+import fastapi
+import pydantic
+
+
+class BoundingBox(pydantic.BaseModel):
+    x: float
+    y: float
+
+
+class FrameResponse(pydantic.BaseModel):
+    sign: str
+    bounding_box: BoundingBox
+
+
+class FramePayload(pydantic.BaseModel):
+    user_id: str  # todo: retrieve this from token
+    frame: fastapi.UploadFile
+    analysis_id: str
+
+
+def map_to_response() -> FrameResponse:
+    return FrameResponse(sign="SPEED_LIMIT_30", bounding_box=BoundingBox(x=100, y=64))
