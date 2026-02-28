f80cae9baff513760eb32306404b749849df7f1d
matiar <110628257+Matiar15@users.noreply.github.com>
2026-02-28 17:54:40 +0100

    #4 feat(config): add MongoDB configuration module
    - Introduced `mongo_config` function for asynchronous Mongo

---
+++ backend/src/infrastructure/config/mongo_config.py
@@ -0,0 +1,30 @@
+import logging
+import typing
+
+import pymongo
+
+from pymongo.asynchronous.database import AsyncDatabase
+
+from backend.src.settings import Settings
+
+_logger = logging.getLogger(__name__)
+
+
+def mongo_config(
+    settings: Settings,
+) -> AsyncDatabase[typing.Mapping[str, typing.Any] | typing.Any]:
+    connection_string = f"mongodb+srv://{settings.database.user}:{settings.database.password}@{settings.database.host}/{settings.database.name}"
+
+    _logger.info(f"Connecting to MongoDB at {settings.database.host}...")
+
+    mongo_client = (
+        pymongo.AsyncMongoClient(
+            connection_string,
+            maxPoolSize=20,
+            maxIdleTimeMS=30_000,
+        )
+    )[settings.database.name]
+
+    _logger.info("Connected to MongoDB.")
+
+    return mongo_client
