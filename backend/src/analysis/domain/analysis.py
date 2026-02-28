644c537aa7eaddd7f2949c8cb24d8e8b0427daa9
matiar <110628257+Matiar15@users.noreply.github.com>
2026-02-28 17:53:54 +0100

    #4 refactor(analysis): rename Metadata model to Analysis and add payload conversion
    - Renamed `Metadata` model to `Analysis` in alignment with domain updates.
    - Added `from_payload` class method to facilitate model instantiation from a dictionary payload.

--- backend/src/analysis/domain/analysis.py
+++ backend/src/analysis/domain/analysis.py
@@ -5,10 +5,15 @@ import pydantic
 PyObjectId = typing.Annotated[str, pydantic.BeforeValidator(str)]
 
 
-class Metadata(pydantic.BaseModel):
+class Analysis(pydantic.BaseModel):
     id: PyObjectId | None = pydantic.Field(alias="_id", default=None)
+    user_id: str
 
     model_config = {
         "populate_by_name": True,
         "extra": "forbid",
     }
+
+    @classmethod
+    def from_payload(cls, payload: dict) -> "Analysis":
+        return cls(**payload)
