78d0d970b74a0981521f564d74e706205d751301
matiar <110628257+Matiar15@users.noreply.github.com>
2026-03-01 20:27:01 +0100

    #4 refactor(app): enhance initialization and improve RustFS integration
    - Added `rustfs_config` for proper configuration of RustFS storage during app initialization.
    - Improved logging to provide clearer insight into application startup and shutdown processes.
    - Refactored lifespan function to streamline initialization of adapters and use cases.

--- backend/src/app.py
+++ backend/src/app.py
@@ -9,17 +9,17 @@ from backend.src.analysis.api.endpoints import analysis_router
 from backend.src.analysis.application.create_analysis_use_case import (
     CreateAnalysisUseCase,
 )
-from backend.src.infrastructure.adapter.rustfs_long_term_storage_adapter import RustFSLongTermStorageAdapter
+from backend.src.infrastructure.adapter.rustfs_long_term_storage_adapter import (
+    RustFSLongTermStorageAdapter,
+)
 from backend.src.infrastructure.adapter.mongo_analysis_adapter import (
     MongoAnalysisAdapter,
-)
-from backend.src.infrastructure.adapter.mongo_frame_adapter import (
-    MongoFrameAdapter,
 )
 from backend.src.infrastructure.config.logging_config import logging_config
 from backend.src.frame.api.endpoints import frames_router
 from backend.src.frame.application.create_frame_use_case import CreateFrameUseCase
 from backend.src.infrastructure.config.mongo_config import mongo_config
+from backend.src.infrastructure.config.rustfs_config import rustfs_config
 from backend.src.settings import get_settings
 
 logging_config()
@@ -31,22 +31,32 @@ _logger = logging.getLogger(__name__)
 async def lifespan(app: fastapi.FastAPI) -> typing.AsyncGenerator[typing.Any]:
     _logger.info("Starting application...")
     settings = get_settings()
+
     mongo_client = mongo_config(settings)
+    long_term_storage_client = rustfs_config(settings)
+    _logger.info("Initialized database ports.")
+
     analysis_port = MongoAnalysisAdapter(mongo_client)
-    long_term_storage_port = RustFSLongTermStorageAdapter(settings)
+    _logger.info("Initialized analysis port.")
+
+    long_term_storage_port = RustFSLongTermStorageAdapter(long_term_storage_client)
+    _logger.info("Initialized long term storage port.")
+
     create_frame_use_case = CreateFrameUseCase(
-        frame_port=MongoFrameAdapter(mongo_client),
         analysis_port=analysis_port,
         long_term_storage_port=long_term_storage_port,
     )
     create_analysis_use_case = CreateAnalysisUseCase(
         analysis_port=analysis_port,
     )
+    _logger.info("Initialized use cases.")
 
     app.state.create_frame_use_case = create_frame_use_case
     app.state.create_analysis_use_case = create_analysis_use_case
 
+    _logger.info("Initialized application.")
     yield
+    _logger.info("Stopping application...")
 
 
 app = fastapi.FastAPI(
