d0b03460c296110b3af02e68fb36a953a8fddc5f
matiar <110628257+Matiar15@users.noreply.github.com>
2026-03-01 13:17:51 +0100

    #4 refactor(adapters): remove `MongoFrameAdapter` and enhance `MongoAnalysisAdapter`
    - Deleted `MongoFrameAdapter` as it is no longer required.
    - Added `update` method and enhanced `get_one` method in `MongoAnalysisAdapter` to support extended filtering and frame updates.

--- backend/src/infrastructure/adapter/mongo_analysis_adapter.py
+++ backend/src/infrastructure/adapter/mongo_analysis_adapter.py
@@ -1,5 +1,6 @@
 import logging
 import typing
+import bson
 
 from pymongo.asynchronous.collection import AsyncCollection
 from pymongo.asynchronous.database import AsyncDatabase
@@ -12,14 +13,14 @@ _logger = logging.getLogger(__name__)
 
 class MongoAnalysisAdapter(AnalysisPort):
     def __init__(
-            self,
-            client: AsyncDatabase[typing.Mapping[str, typing.Any] | typing.Any],
+        self,
+        client: AsyncDatabase[typing.Mapping[str, typing.Any] | typing.Any],
     ):
         self.client: AsyncCollection[Analysis] = client["analysis"]
 
     async def create(
-            self,
-            analysis: Analysis,
+        self,
+        analysis: Analysis,
     ) -> str:
         _logger.info(f"Creating analysis for user: {analysis.user_id}...")
         insert_one_result = await self.client.insert_one(
@@ -34,12 +35,33 @@ class MongoAnalysisAdapter(AnalysisPort):
 
         return inserted_id
 
-
-    async def get_one(self, id: str, user_id: str) -> Analysis | None:
+    async def get_one(
+        self, id: str, user_id: str, statuses: list[str] | None = None
+    ) -> Analysis | None:
         _logger.info(f"Getting analysis with id: {id}...")
-        analysis = await self.client.find_one({"_id": id, "user_id": user_id})
+
+        filter_criteria: dict[str, typing.Any] = {
+            "_id": bson.ObjectId(id),
+            "user_id": user_id,
+        }
+        if statuses:
+            filter_criteria["status"] = {"$in": statuses}
+
+        analysis = await self.client.find_one(filter_criteria)
 
         if analysis:
             _logger.info(f"Analysis found: {analysis}")
 
-        return analysis
+        return Analysis.model_validate(analysis)
+
+    async def update(self, id: str, **kwargs: typing.Any) -> None:
+        updates = dict()
+        frame = kwargs.pop("frame", None)
+
+        if frame:
+            updates.update({"$push": {"frames": frame.model_dump(by_alias=True, exclude_unset=True)}})
+
+        await self.client.update_one(
+            filter={"_id": bson.ObjectId(id)},
+            update=updates
+        )
