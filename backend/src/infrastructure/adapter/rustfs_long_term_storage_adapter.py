65773484c9cc39f474a6c131c95a5d91d6fc4fe4
matiar <110628257+Matiar15@users.noreply.github.com>
2026-03-01 20:27:51 +0100

    #4 refactor(storage): decouple RustFS client initialization and streamline adapter
    - Introduced `rustfs_config` to handle centralized RustFS client configuration.
    - Refactored `RustFSLongTermStorageAdapter` to accept an external client for improved modularity and testability.

--- backend/src/infrastructure/adapter/rustfs_long_term_storage_adapter.py
+++ backend/src/infrastructure/adapter/rustfs_long_term_storage_adapter.py
@@ -2,35 +2,34 @@ import asyncio
 import typing
 import uuid
 
-import boto3
-from botocore.config import Config
+from botocore.client import BaseClient
 
-from backend.src.long_term_storage.application.long_term_storage_port import LongTermStoragePort
-from backend.src.settings import Settings
+from backend.src.long_term_storage.application.long_term_storage_port import (
+    LongTermStoragePort,
+)
 
 
 class RustFSLongTermStorageAdapter(LongTermStoragePort):
-    def __init__(self, settings: Settings):
-        self.client = boto3.client(
-            "s3",
-            endpoint_url=settings.long_term_storage.url,
-            aws_access_key_id=settings.long_term_storage.user,
-            aws_secret_access_key=settings.long_term_storage.password,
-            config=Config(signature_version="s3v4"),
-        )
+    def __init__(self, client: BaseClient):
+        self.client = client
 
-    async def store_file(self, file: typing.BinaryIO, bucket_name: str, naming_strategy: str,) -> str:
+    async def store_file(
+        self,
+        file: typing.BinaryIO,
+        bucket_name: str,
+        naming_strategy: str,
+    ) -> str:
         file_id = str(uuid.uuid4())
         object_name = f"{naming_strategy}_{file_id}.jpg"
 
         await asyncio.to_thread(
-            self.client.upload_fileobj,
+            self.client.upload_fileobj,  # type: ignore
             Fileobj=file,
             Bucket=bucket_name,
             Key=object_name,
             ExtraArgs={
                 "ContentType": "image/jpeg",
-            }
+            },
         )
 
         return object_name
