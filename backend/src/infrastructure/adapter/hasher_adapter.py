89bf95f25c5dc699fb55fa6ba5991aef2374d615
matiar <110628257+Matiar15@users.noreply.github.com>
2026-03-14 21:15:08 +0100

    #24-add-register-and-login-for-auth | feat(Hasher): Implement hasher adapter

---
+++ backend/src/infrastructure/adapter/hasher_adapter.py
@@ -0,0 +1,15 @@
+import pwdlib
+
+from backend.src.hasher.application.hasher_port import HasherPort
+
+class HasherAdapter(HasherPort):
+    def __init__(
+        self,
+    ):
+        self.password_hash = pwdlib.PasswordHash.recommended()
+
+    async def hash(self, text: str) -> str:
+        return self.password_hash.hash(text)
+
+    async def verify(self, text: str, hashed_text: str) -> bool:
+        return self.password_hash.verify(text, hashed_text)
