f54a3daf9df2a184bb99740d1cddb37b3b9ccd66
matiar <110628257+Matiar15@users.noreply.github.com>
2026-03-14 22:09:03 +0100

    #24-add-register-and-login-for-auth | feat(Authentication): Implement authentication adapter with user and password hashing

---
+++ backend/src/infrastructure/adapter/authentication_adapter.py
@@ -0,0 +1,26 @@
+from backend.src.authentication.application.authentication_port import AuthenticationPort
+from backend.src.hasher.application.hasher_port import HasherPort
+from backend.src.user.application.user_port import UserPort
+
+
+class AuthenticationAdapter(AuthenticationPort):
+    def __init__(
+        self,
+        user_port: UserPort,
+        password_hasher: HasherPort,
+    ):
+        self.user_port = user_port
+        self.password_hasher = password_hasher
+
+    async def authenticate(
+        self, email: str, password: str, token: str,
+    ) -> bool:
+        user = await self.user_port.fetch(email, password)
+
+        if not user:
+            raise Exception("Invalid credentials.")
+
+        if not self.password_hasher.verify(password, user.password):
+            return False
+
+        return True
